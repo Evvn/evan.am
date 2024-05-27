73e3f0f1a1ed117e3e6ced0e4b5eb2a0d73b8999
Evan Amezcua <evvn@outlook.com>
2024-05-27 22:36:47 +0900

    im an editor

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <div className={styles.homeContainer}>
-      <p>Evan Amezcua ☺ software engineer</p>
+      <p>Evan Amezcua ☺ video editor & web dev</p>
       <a href='tel:07013940013'>☏ 070 1394 0013</a>
       <a href='mailto:hello@evan.am'>✉ hello@evan.am</a>
       <p>🜨 Tokyo, Japan</p>
