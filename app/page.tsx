86358d72aee9a659a9894bdbfc614b6a83d23a19
Evan Amezcua <evvn@outlook.com>
2024-08-11 00:31:17 +0900

    editing portfolio v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,106 @@
+'use client';
 import styles from './page.module.css';
+import { useState } from 'react';
+
+const projects = [
+  {
+    title: 'YUMMY – NENE ft. YOUNG COCO',
+    description: '',
+    src: 'https://streamable.com/e/v3862l?autoplay=1&muted=1',
+  },
+  {
+    title: 'Do It Like Me – Yurufuwa Gang x Glogang',
+    description: '',
+    src: 'https://streamable.com/e/bqtzry?autoplay=1&muted=1',
+  },
+  {
+    title: 'The Nunchuck Princess',
+    description: '',
+    src: 'https://streamable.com/e/rzo9y0?autoplay=1&muted=1',
+  },
+  {
+    title: 'Phoebes Angels – adidas Forum',
+    description: '',
+    src: 'https://streamable.com/e/ez43x5?autoplay=1&muted=1',
+  },
+  {
+    title: 'eye_C magazine x peacebird',
+    description: '',
+    src: 'https://streamable.com/e/8xlrhl?autoplay=1&muted=1',
+  },
+];
 
 export default function Home() {
+  const [currentProject, setCurrentProject] = useState(0);
+
   return (
     <div className={styles.homeContainer}>
-      <p>Evan Amezcua ☺ video editor & web dev</p>
-      <a href='tel:07013940013'>☏ 070 1394 0013</a>
-      <a href='mailto:hello@evan.am'>✉ hello@evan.am</a>
-      <p>🜨 Tokyo, Japan</p>
+      {/* -------- NAV --------  */}
+      <nav className={styles.homeNav}>
+        <div>
+          <h1>EVAN.AM</h1>
+          <p>editor | tokyo</p>
+        </div>
+
+        <div className={styles.projectList}>
+          {projects.map((project, i) => (
+            <p
+              key={project.title}
+              onClick={() => {
+                setCurrentProject(i);
+              }}
+              style={{
+                cursor: 'pointer',
+                textDecoration: i === currentProject ? 'underline' : 'none',
+                marginBottom: '.5rem',
+              }}
+            >
+              {i + 1}. {project.title}
+            </p>
+          ))}
+        </div>
+
+        <div>
+          {/* <p>
+            <a href='tel:07013940013'>☏ 070 1394 0013</a>
+          </p> */}
+          {/* <p>
+            <a href='mailto:hello@evan.am'>✉ hello@evan.am</a>
+          </p> */}
+          <p>
+            <a href='mailto:hello@evan.am'>hello@evan.am</a> |{' '}
+            <a href='https://instagram.com/evanamezcua'>@evanamezcua</a>
+          </p>
+          {/* <p>🜨 Tokyo, Japan</p> */}
+        </div>
+      </nav>
+      {/* -------- NAV --------  */}
+
+      {/* -------- VIDEO --------  */}
+      <div className={styles.videoContainer}>
+        <iframe
+          allow='fullscreen;autoplay'
+          height='100%'
+          src={projects[currentProject].src}
+          width='100%'
+          className={styles.iframe}
+        />
+      </div>
+      {/* -------- VIDEO --------  */}
+
+      {/* {projects.map((project) => (
+        <div key={project.title} style={{ width: '10vw' }}>
+          <div className={styles.videoContainer}>
+            <iframe
+              allow='fullscreen;autoplay'
+              height='100%'
+              src={project.src}
+              width='100%'
+              className={styles.iframe}
+            />
+          </div>
+        </div>
+      ))} */}
     </div>
   );
 }
